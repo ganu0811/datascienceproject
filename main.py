74c5a28ca6924ec7e3089e01eadc882e384046d4
ganu0811 <ganufindsu@gmail.com>
2024-12-28 14:22:24 +0000

    Data Transformation Pipeline added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.datascience import logger
 
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 STAGE_NAME = " Data Ingestion Stage"
 
 try:
@@ -29,3 +29,19 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<')
+
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
+                
---
+++ src/datascience/components/data_transformation.py
@@ -0,0 +1,39 @@
+import os
+from src.datascience import logger
+from src.datascience.entity.config_entity import (DataTransformationConfig)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class DataTransformation:
+    
+    def __init__(self, config:DataTransformationConfig):
+        
+        self.config = config
+
+        # Note: Different data transformation techniques such as Scaler, PCA and others can be added here
+        # All kinds of EDA in ML Cycle are performed here before passing this data to the model
+        
+    
+    def train_test_splitting(self):
+        
+        data = pd.read_csv(self.config.data_path)
+        
+        # Splitting the data into train and test
+        
+        train, test = train_test_split(data)
+        
+        train.to_csv(os.path.join(self.config.root_dir, 'train.csv'), index = False)
+        test.to_csv(os.path.join(self.config.root_dir, ' test.csv'), index = False)
+        
+        
+        logger.info('Splitted data into training and testing')
+        logger.info(train.shape)
+        logger.info(test.shape)
+        
+        print(train.shape)
+        print(test.shape)
+        
+        
+        
+        
